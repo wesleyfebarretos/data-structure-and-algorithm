914a536e0c7015834eee078a0abdfff71fe9c0af
Wesley Ferreira <wesleyfebarretos@gmail.com>
2024-11-14 00:17:12 -0300

    chore: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,11 +3,6 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      isolatedModules: true, // speeds up testing by isolating modules (useful for large projects)
-    },
-  },
   moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest', // Handles transforming TypeScript files
