1ce5685cee521475fa5776400c4001e8fa7267a1
Wesley Ferreira <wesleyfebarretos@gmail.com>
2024-11-14 00:51:03 -0300

    test: improve heap tests

--- __test__/MinHeap.spec.ts
+++ __test__/MinHeap.spec.ts
@@ -12,12 +12,20 @@ describe("Min Heap", () => {
         minHeap.insert(-10);
 
 
+        expect(minHeap.length).toEqual(6);
         expect(minHeap.delete()).toEqual(-10);
+        expect(minHeap.length).toEqual(5);
         expect(minHeap.delete()).toEqual(1);
+        expect(minHeap.length).toEqual(4);
         expect(minHeap.delete()).toEqual(10);
+        expect(minHeap.length).toEqual(3);
         expect(minHeap.delete()).toEqual(29);
+        expect(minHeap.length).toEqual(2);
         expect(minHeap.delete()).toEqual(29);
+        expect(minHeap.length).toEqual(1);
         expect(minHeap.delete()).toEqual(30);
+        expect(minHeap.length).toEqual(0);
         expect(minHeap.delete()).toEqual(null);
+        expect(minHeap.length).toEqual(0);
     })
 })
